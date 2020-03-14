959050088df6a806df82c9ea6801ede0c0615f4e
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-03-14 09:06:56 -0400

    Update icojs.js

--- assets/js/icojs.js
+++ assets/js/icojs.js
@@ -9,7 +9,10 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/12Lyd3k0PRpe-Ie_LaeJaEPm3F
         var cntry_case = data.feed.entry[i]['gsx$icontotcasepercntry']['$t'];
         var ico = data.feed.entry[i]['gsx$icon']['$t'];
         var dth = data.feed.entry[i]['gsx$dthcntry']['$t'];
+          var chc = data.feed.entry[i]['gsx$chc']['$t'];
+  var cdt = data.feed.entry[i]['gsx$chd']['$t'];
 
-        document.getElementById('ico_cntry').innerHTML += ('<div class="box box__second">'+'<svg class="avatar avatar--120">'+'<g class="avatar__box">'+'<image xlink:href="https://tamimi-dev.github.io/mobapp/assets/icons/cntry/'+ico+'.svg"'+' height="100%"'+'width="100%"></image></g></svg><div class="text-center"><h4 class="title title--h4 sidebar__user-name">'+cntry+'</h4></div><div class="text-center"><table class="table5" align="center"><tr><th>Cases</th><th>Deaths</th></tr><tr><td>'+cntry_case+'</td><td>'+dth+'</td></tr></table></div></div>');
+    document.getElementById('ico_cntry').innerHTML += ('<div class="col-lg-4 col-xl-4">'+'<div class="box box__second ">'+'<svg class="avatar avatar--120">'+'<g class="avatar__box">'+'<image xlink:href="https://tamimi-dev.github.io/app/assets/icons/cntry/'+ico+'.svg"'+' height="100%"'+'width="100%"></image></g></svg><div class="text-center"><h4 class="title title--h4 sidebar__user-name">'+cntry+'</h4></div><div class="text-center"><table class="table5" align="center"><tr><th>Cases</th><th>Deaths</th></tr><tr><td>'+cntry_case+'</td><td>'+dth+'</td></tr><tr><td>'+chc+'</td><td>'+cdt+'</td></tr></table></div></div></div>');
+  
       }
-    });
+    });   
